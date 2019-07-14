a482438d486f11b079c904fa833549fe6a167f33
Sang-Kil Park <likejazz@gmail.com>
2019-07-14 14:30:56 +0900

    .

---
+++ leetcode/14-longest-common-prefix.py
@@ -0,0 +1,18 @@
+# 14-longest-common-prefix.py
+#
+# Copyright (C) 2019 Sang-Kil Park <likejazz@gmail.com>
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs is None or len(strs) == 0:
+            return ""
+        pre = strs[0]
+        # Compare all strings by traversing.
+        for i in range(1, len(strs)):
+            while not (strs[i].startswith(pre)):
+                pre = pre[:len(pre) - 1]
+        return pre
+
