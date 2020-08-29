63dd84bf610928f24fb9b4edd932070f1f29a8cd
Sang-Kil Park <likejazz@gmail.com>
2020-08-29 21:15:56 +0900

    .

---
+++ go/1021-remove-outermost-parentheses.go
@@ -0,0 +1,22 @@
+func removeOuterParentheses(S string) string {
+	var stack = list.New()
+	var new []string
+
+	for _, ch := range S {
+
+		if stack.Len() == 0 {
+			stack.PushBack(ch)
+			continue
+		} else if string(ch) == "(" {
+			stack.PushBack(ch)
+		} else if string(ch) == ")" {
+			stack.Remove(stack.Back())
+			if stack.Len() == 0 {
+				continue
+			}
+		}
+
+		new = append(new, string(ch))
+	}
+	return strings.Join(new, "")
+}
